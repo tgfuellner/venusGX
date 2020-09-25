9e4374fea1000920e9eed0d28010575e5a65552f
Thomas Gfüllner <thomas.gfuellner@gmail.com>
2020-09-25 10:44:42 +0200

    chore: Utils mit echo

--- copyFromVenus.sh
+++ copyFromVenus.sh
@@ -1,1 +1,2 @@
+echo "scp root@192.168.0.181:setGoePower.py ."
 scp root@192.168.0.181:setGoePower.py .
--- copytoVenus.sh
+++ copytoVenus.sh
@@ -1,1 +1,2 @@
+echo "scp setGoePower.py root@192.168.0.181:setGoePower.py"
 scp setGoePower.py root@192.168.0.181:setGoePower.py
