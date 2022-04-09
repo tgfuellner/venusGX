441ba8f4e3b3bb86f8247260de1fef92b4cb6885
Thomas Gfüllner <thomas.gfuellner@gmail.com>
2022-04-09 16:35:17 +0200

    feat: set power

--- goe.py
+++ goe.py
@@ -1,6 +1,22 @@
 #!/usr/bin/python
 
+import sys
+from datetime import datetime
 from goecharger import (GoeCharger)
 
-status = GoeCharger('192.168.0.126').requestStatus()
-print("gerade geladen %.2f    gesamt %.2f" % (status['current_session_charged_energy'], status['energy_total']))
+now = datetime.now()
+
+goeCharger = GoeCharger('192.168.0.126')
+
+status = goeCharger.requestStatus()
+print("%s gerade geladen %.2fkWh    gesamt %.2fkWh mit %sA" % (now.strftime("%H:%M"), status['current_session_charged_energy'], status['energy_total'], status['charger_max_current']))
+
+if len(sys.argv) == 2:
+    current = int(sys.argv[-1])
+    if current == 0:
+        goeCharger.setAllowCharging(0)
+        print("  Pausiere den Ladevorgang")
+    else:
+        goeCharger.setAllowCharging(1)
+        status = goeCharger.setMaxCurrent(current)
+        print("  %sA gesetzt, charger Temp=%sC" % (status['charger_max_current'], status['charger_temp']))
