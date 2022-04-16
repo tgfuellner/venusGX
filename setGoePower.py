27e40d5f2318578292c6b703317dcccf11ee5bee
Thomas Gfüllner <thomas.gfuellner@gmail.com>
2022-04-16 14:28:14 +0200

    chore: Default Max current 22

--- setGoePower.py
+++ setGoePower.py
@@ -25,7 +25,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-b", "--battery_offset", help="Solar Battery Offset in Watt", type=int, default=0)
 parser.add_argument("-s", "--solar_soc", help="Keep Solar Battery State of Charge in Percent", type=int, default=20)
 parser.add_argument("-i", "--goe_ampere", help="Charge with given ampere", type=int, default=0)
-parser.add_argument("-m", "--goe_max_ampere", help="Do not use more ampere for charging", type=int, default=20)
+parser.add_argument("-m", "--goe_max_ampere", help="Do not use more ampere for charging", type=int, default=22)
 parser.add_argument("-k", "--keep_charging", help="Dont stop charging, usfull if car cant be woken up", action="store_true")
 parser.add_argument("-v", "--verbose", help="increase output verbosity", action="store_true")
 args = parser.parse_args()
@@ -96,7 +96,7 @@ def setAmpere(i):
         try:
             goeCharger.setAllowCharging(1)
             status = goeCharger.setMaxCurrent(i)
-            print("--> %d Ampere erfolgreich gesetzt um %s" % (i, currentTime))
+            print("--> %d Ampere gesetzt um %s" % (i, currentTime))
             return True
         except Exception:
             print("Nicht gesetzt! %s" % i)
